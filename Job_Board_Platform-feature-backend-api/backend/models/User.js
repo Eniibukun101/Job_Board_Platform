52188ac4c692779260dc7fc6a30c0a41740058a2
Karinaabulu <karina.abulu@pau.edu.ng>
2026-05-14 08:27:27 +0100

    chore: remove incorrectlyt named .env.exmple file

---
+++ Job_Board_Platform-feature-backend-api/backend/models/User.js
@@ -0,0 +1,15 @@
+const { DataTypes } = require('sequelize');
+const sequelize = require('../config/database');
+
+const User = sequelize.define('User', {
+  id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
+  name: { type: DataTypes.STRING, allowNull: false },
+  email: { type: DataTypes.STRING, allowNull: false, unique: true, validate: { isEmail: true } },
+  password: { type: DataTypes.STRING, allowNull: false },
+  userType: { type: DataTypes.ENUM('Applicant', 'Employer'), defaultValue: 'Applicant' },
+  company: { type: DataTypes.STRING, allowNull: true },
+  phone: { type: DataTypes.STRING, allowNull: true },
+  bio: { type: DataTypes.TEXT, allowNull: true }
+}, { timestamps: true });
+
+module.exports = User;
